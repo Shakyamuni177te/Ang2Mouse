422e52b27871fc82f8686f0d253f452ace100168
Shakyamuni177te <orderofthemouse@gmail.com>
2016-09-28 11:40:01 +0000

    Added sample id and character name

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,24 @@
 import { Component } from '@angular/core';
 
+export class character {
+  id: number;
+  name: string;
+}
+
 @Component({
-    selector: 'my-app',
-    template: '<h1>Order Of The Mouse: Operation Dovecot</h1>'
+    selector: 'mouse-app',
+    template: `<h1>{{title}}</h1>
+               <h2>{{sectiontitle}}</h2>
+               <h3></h3>
+               <div><label>id: {{character.id}}</label></div>
+               <div><label>name: {{character.name}}</label></div>`
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = 'Order Of The Mouse: Operation Dovecot';
+  sectiontitle = 'Character Selection';
+    character: Character = {
+    id: 1,
+    name: 'Dragon-Bear'
+  };
+}
