4b32e9991c8fdce55bda8d4449f8321642dd70f0
Shakyamuni177te <peter.carter.mail@gmail.com>
2016-10-12 05:45:12 +0000

    added comments

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,13 +1,19 @@
+// import global modules
 import { ModuleWithProviders }  from '@angular/core';
+// import routers
 import { Routes, RouterModule } from '@angular/router';
 
+// import mission component
 import { MissionsComponent }      from './missions.component';     
 
+// import character and character details component
 import { CharactersComponent }     from './characters/characters.component';
 import { CharacterDetailComponent} from './characters/character-detail.component';
 
+// import bases component
 import { BasesComponent } from './bases/bases.component';
 
+// basic application routing (which URL maps to which component)
 const appRoutes: Routes = [
   {
     path: '',
