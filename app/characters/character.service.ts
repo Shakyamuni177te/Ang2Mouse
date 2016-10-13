dd01f0ce39dc61409c63b5007d706c08914a8d89
Peter David Carter <orderofthemouse@gmail.com>
2016-10-13 18:10:22 +0100

    spacing fix

--- app/characters/character.service.ts
+++ app/characters/character.service.ts
@@ -1,7 +1,7 @@
 import { Injectable }                              from '@angular/core';
 import { Headers, Http, Response, RequestOptions } from '@angular/http';
 
-import { Observable }     from 'rxjs/Observable';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
 
 import { Character } from './character';
