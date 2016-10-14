77f5d200b8e0fca9271f80a7fe2a22994f039aa0
Shakyamuni177te <peter.carter.mail@gmail.com>
2016-10-14 08:08:34 +0000

    added configurable authentication

--- app/auth.service.ts
+++ app/auth.service.ts
@@ -1,73 +1,70 @@
-// import injection, routing and authentication modules
-import { Injectable } from '@angular/core';
-
+import { Injectable }      from '@angular/core';
 import { tokenNotExpired } from 'angular2-jwt';
+import { Router }          from '@angular/router';
+import { myConfig }        from './auth.config';
 
-import { Router } from '@angular/router';
-
-//variable contains Auth0Lock data and functions
-declare var Auth0Lock: any;
+// Avoid name not found warnings
+declare var Auth0: any;
 
 @Injectable()
 export class AuthService {
+  // Configure Auth0
+  auth0 = new Auth0({
+    domain: myConfig.domain,
+    clientID: myConfig.clientID,
+    callbackOnLocationHash: true,
+    callbackURL: myConfig.callbackURL,
+  });
 
-  lock = new Auth0Lock('', '');
-  
-  // create authentication routing
   constructor(private router: Router) {
+    var result = this.auth0.parseHash(window.location.hash);
 
-    this.lock.on('authenticated', (authResult: any) => {
+    if (result && result.idToken) {
+      localStorage.setItem('id_token', result.idToken);
+      this.router.navigate(['/missions']);
+    } else if (result && result.error) {
+      alert('error: ' + result.error);
+    }
+  }
 
-      localStorage.setItem('id_token', authResult.idToken);
-
-      this.lock.getProfile(authResult.idToken, (error: any, profile: any) => {
-
-        if (error) {
-
-          console.log(error);
-
-        }
-
-        localStorage.setItem('profile', JSON.stringify(profile));
-
-      });
-
-      this.lock.hide();
-
+  public signUp(username: string, password: string) {
+    this.auth0.signup({
+      connection: 'Username-Password-Authentication',
+      responseType: 'token',
+      email: username,
+      password: password,
+    }, function(err: any) {
+      if (err) alert("something went wrong: " + err.message);
     });
+  };
 
-  }
-  
-  // login function
-  login() {
+  public login(username: string, password: string) {
+    this.auth0.login({
+      connection: 'Username-Password-Authentication',
+      responseType: 'token',
+      email: username,
+      password: password,
+    }, function(err: any) {
+      if (err) alert("something went wrong: " + err.message);
+    });
+  };
 
-    this.lock.show();
+  public googleLogin() {
+    this.auth0.login({
+      connection: 'google-oauth2'
+    }, function(err: any) {
+      if (err) alert("something went wrong: " + err.message);
+    });
+  };
 
-  }
-  
-
-  // logout function
-  logout() {
-
-    // To log out, just remove the token and profile
-
-    // from local storage
-
-    localStorage.removeItem('profile');
-
-    localStorage.removeItem('id_token');
-
-    // Send the user back to the dashboard after logout
-
-    this.router.navigateByUrl('/missions');
-
-  }
-  
-  // check to see if a user is already logged in
-  loggedIn() {
-
+  public loggedIn() {
+    // Check if there's an unexpired JWT
+    // It searches for an item in localStorage with key == 'id_token'
     return tokenNotExpired();
+  };
 
-  }
-
+  public logout() {
+    // Remove token from localStorage
+    localStorage.removeItem('id_token');
+  };
 }
